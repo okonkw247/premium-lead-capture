ede9c19ca44e08fde65a07b40abfdfc42e066ed9
shadow-operator <okonkw247@gmail.com>
2026-08-16 05:28:04 +0100

    build: update Next.js development build artifacts and index.html structure

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-74dfddd520561279.js","static/chunks/fd9d1056-3eee857bde8f3b06.js","static/chunks/117-29466efefd806f9c.js","static/chunks/main-app-3442a942d1599413.js"],pages:{"/_app":["static/chunks/webpack-74dfddd520561279.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-1357e995d27cb14f.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-74dfddd520561279.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-1357e995d27cb14f.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
